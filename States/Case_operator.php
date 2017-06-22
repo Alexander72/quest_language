de785b35c232eb549d2d38eae4fc9d709fd0ba8e
Alexander <alesander1996@gmail.com>
2017-06-22 19:05:15 +0200

    extends Recursive_operator

--- States/Case_operator.php
+++ States/Case_operator.php
@@ -6,124 +6,104 @@
  * Time: 6:24
  */
 
-class Case_operator extends Primitive_operator
+class Case_operator extends Recursive_operator
 {
     private $case_source;
     private $case_title;
-    private $result = [];
-    private $state;
     private $bracket_counter = 0;
-
-    private $line;
-    private $pos;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->state = 'DEFAULT';
-    }
-
 
     protected function get_operator()
     {
         return 'CASE';
     }
 
-    public function add_symbol($symbol)
+    protected function set_states()
     {
-        global $debug;
-        if ($this->state == 'DEFAULT' && trim($symbol))
-        {
-            if($symbol == '{') {
-                $this->state = 'CASE_TITLE';
-            }
-             else {
-                 throw new MY_Exception("Unexpected '".$symbol."'. Expected '{'");
-             }
+        $this->states = [
+            'DEFAULT' => ['start' => true, 'method' => 'default_state'],
+            'CASE_TITLE' => ['method' => 'case_title_state'],
+            'WAIT_CASE' => ['method' => 'wait_case_state'],
+            'CASE' => ['recursive' => true, 'method' => 'case_state', 'need_trim' => false],
+            'END' => ['finish' => true, 'method' => 'end_state'],
+        ];
+    }
 
+    protected function default_state($symbol)
+    {
+        if($symbol == '{') {
+            $this->set_state('CASE_TITLE');
         }
-        elseif($this->state == 'CASE_TITLE' && trim($symbol))
-        {
-            if($symbol == ':') {
-                if(trim($this->case_title))
-                    if(preg_match('/"(.*)"/', $this->case_title, $matches)){
-                        $this->case_title = $matches[1];
-                        $this->state = 'WAIT_CASE';
-                    }
-                    else{
-                        throw new MY_Exception("Incorrect case name");
-                    }
-                else
-                    throw new MY_Exception("Unexpected ':'. Expected case name");
-            }
-            elseif($symbol == '}') {
-                $this->result_ready = true;
-                $this->state = 'END';
-                $this->operator->set_default();
-            }
-            elseif($symbol == "\n")
-            {
-                throw new MY_Exception("Unexpected \\n. Expected ':'");
-            }
-            else
-                $this->case_title .= $symbol;
+        else {
+            throw new MY_Exception("Unexpected '".$symbol."'. Expected '{'");
         }
-        elseif($this->state == 'WAIT_CASE' && trim($symbol))
-        {
-            if($symbol == '{') {
-                $this->line = $debug->get_line();
-                $this->pos = $debug->get_position() + 1;
-                $this->state = 'CASE';
-            }
-            else
-                throw new MY_Exception("Unexpected '".$symbol."'. Expected '{'");
+    }
 
-        }
-        elseif($this->state == 'CASE')
-        {
-            if($symbol == '}')
-                $this->bracket_counter--;
-            
-            if($symbol == '{')
-                $this->bracket_counter++;
-
-            if($this->bracket_counter == -1) {
-                if(trim($this->case_source)) {
-                    $this->operator->set_default();
-                    $this->result['value'][$this->case_title] = parse($this->case_source, 1);
-                    $this->case_source = '';
-                    $this->case_title = '';
-                    $this->bracket_counter = 0;
-                    $debug->new_line($this->line);
-                    $debug->new_pos($this->pos);
-                    $this->state = 'CASE_TITLE';
-                    $this->operator->set_operator('CASE');
+    protected function case_title_state($symbol)
+    {
+        if($symbol == ':') {
+            if(trim($this->case_title))
+                if(preg_match('/"(.*)"/', $this->case_title, $matches)){
+                    $this->case_title = $matches[1];
+                    $this->set_state('WAIT_CASE');
                 }
                 else{
-                    throw new MY_Exception("Unexpected '".$symbol."'");
+                    throw new MY_Exception("Incorrect case name");
                 }
+            else
+                throw new MY_Exception("Unexpected ':'. Expected case name");
+        }
+        elseif($symbol == '}') {
+            $this->result_ready = true;
+            $this->set_state('END');
+            $this->operator->set_default();
+        }
+        elseif($symbol == "\n")
+        {
+            throw new MY_Exception("Unexpected \\n. Expected ':'");
+        }
+        else
+            $this->case_title .= $symbol;
+    }
+
+    protected function wait_case_state($symbol)
+    {
+        if($symbol == '{') {
+            $this->line = $this->debug->get_line();
+            $this->pos = $this->debug->get_position() + 1;
+            $this->set_state('CASE');
+        }
+        else
+            throw new MY_Exception("Unexpected '".$symbol."'. Expected '{'");
+    }
+
+    protected function case_state($symbol)
+    {
+        if($symbol == '}')
+            $this->bracket_counter--;
+
+        if($symbol == '{')
+            $this->bracket_counter++;
+
+        if($this->bracket_counter == -1) {
+            if(trim($this->case_source)) {
+                $this->operator->set_default();
+                $res = parse($this->case_source, 1);
+                $this->result['value'][$this->case_title] = $res;
+                $this->case_source = '';
+                $this->case_title = '';
+                $this->bracket_counter = 0;
+                $this->debug->new_line($this->line);
+                $this->debug->new_pos($this->pos);
+                $this->set_state('CASE_TITLE');
+                $this->operator->set_operator('CASE');
             }
             else{
-                $this->case_source .= $symbol;
+                throw new MY_Exception("Unexpected '".$symbol."'");
             }
-
         }
-        elseif(trim($symbol)){
-            throw new MY_Exception("Unexpected '".$symbol."'");
+        else{
+            $this->case_source .= $symbol;
         }
-    }
-
-    public function result_ready()
-    {
-        return $this->state == 'END';
     }
     
-    public function get_result()
-    {
-        $res = $this->result;
-        $res['operator'] = 'CASE';
-        $this->state = 'DEFAULT';
-        $this->operator->set_default();
-        return $res;
-    }
 }
